b719000bc354bc7765c97868488b8f9ec83cf25b
Arunkumar <guptaarunkumar060@gmail.com>
2024-08-24 08:05:03 +0530

    update hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -13,8 +13,11 @@ function Hero() {
             <h2 className='title'>WELCOME TO YASH SYSTEMS</h2>
             <div className="text-image">
                 <p>Welcome to SBR COMPUTERS laptop repair in Chanda Nagar, you are at the right place to service your laptop with the best price and quality, since 2009.
+                    <br /><br />
                     We are the team of highly skilled and qualified technicians for all your laptop repairs, our company is one of the best laptop repair services in Chanda Nagar and across Hyderabad.
+                    <br /><br />
                     We offer Affordable and Reasonable prices and we fix all kind of major issues, quick response, within a hour. We can give solution and we fix at your home, office and anywhere in Hyderabad.
+                    <br /><br />
                     We repair all kind of major brands like Dell, HP, Lenovo, Acer, Apple, Compaq, Toshiba, Sony, Samsung, Asus. Whenever you want to have laptop repair supports just call us.
                 </p>
                 <img src={hero_2} className='hero_2' alt="" />
